36b1184fe33a477d01b57906f95ec93a79fc075f
voxx <admin@axe.io>
2018-08-18 17:37:59 -0700

    Fix reCAPTCHA alignment.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -91,8 +91,8 @@
 
 
                         @if(config('settings.reCaptchStatus'))
-                            <div class="form-group">
-                                <div class="col-sm-6 col-sm-offset-4">
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
                                     <div class="g-recaptcha" data-sitekey="{{ config('settings.reCaptchSite') }}"></div>
                                 </div>
                             </div>
