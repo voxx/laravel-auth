a290052435dc9b11cf99658373a959ff4ab384f1
voxx <github@axe.io>
2020-10-22 17:43:54 -0700

    Use laravel-auth version of UsersTableSeeder.php instead of old/broken version pulled from laravel-roles v4.0

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use jeremykenedy\LaravelRoles\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,36 +17,58 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $userRole = config('roles.models.role')::where('name', '=', 'User')->first();
-        $adminRole = config('roles.models.role')::where('name', '=', 'Admin')->first();
-        $permissions = config('roles.models.permission')::all();
+        $profile = new Profile();
+        $adminRole = Role::whereName('Admin')->first();
+        $userRole = Role::whereName('User')->first();
 
-        /*
-         * Add Users
-         *
-         */
-        if (config('roles.models.defaultUser')::where('email', '=', 'admin@admin.com')->first() === null) {
-            $newUser = config('roles.models.defaultUser')::create([
-                'name'     => 'Admin',
-                'email'    => 'admin@admin.com',
-                'password' => bcrypt('password'),
+        // Seed test admin
+        $seededAdminEmail = 'admin@admin.com';
+        $user = User::where('email', '=', $seededAdminEmail)->first();
+        if ($user === null) {
+            $user = User::create([
+                'name'                           => 'Admin',
+                'first_name'                     => 'Harry',
+                'last_name'                      => 'Potter',
+                'email'                          => $seededAdminEmail,
+                'password'                       => Hash::make('password'),
+                'token'                          => str_random(64),
+                'activated'                      => true,
+                'signup_confirmation_ip_address' => '127.0.0.1',
+                'admin_ip_address'               => '127.0.0.1',
             ]);
 
-            $newUser->attachRole($adminRole);
-            foreach ($permissions as $permission) {
-                $newUser->attachPermission($permission);
-            }
+            $user->profile()->save($profile);
+            $user->attachRole($adminRole);
+            $user->save();
         }
 
-        if (config('roles.models.defaultUser')::where('email', '=', 'user@user.com')->first() === null) {
-            $newUser = config('roles.models.defaultUser')::create([
-                'name'     => 'User',
-                'email'    => 'user@user.com',
-                'password' => bcrypt('password'),
+        // Seed test user
+        $user = User::where('email', '=', 'user@user.com')->first();
+        if ($user === null) {
+            $user = User::create([
+                'name'                           => 'User',
+                'first_name'                     => 'Hermione',
+                'last_name'                      => 'Granger',
+                'email'                          => 'user@user.com',
+                'password'                       => Hash::make('password'),
+                'token'                          => str_random(64),
+                'activated'                      => true,
+                'signup_ip_address'              => '127.0.0.1',
+                'signup_confirmation_ip_address' => '127.0.0.1',
             ]);
 
-            $newUser;
-            $newUser->attachRole($userRole);
+            $user->profile()->save(new Profile());
+            $user->attachRole($userRole);
+            $user->save();
         }
+
+        // Seed test users
+        // $user = factory(App\Models\Profile::class, 5)->create();
+        // $users = User::All();
+        // foreach ($users as $user) {
+        //     if (!($user->isAdmin()) && !($user->isUnverified())) {
+        //         $user->attachRole($userRole);
+        //     }
+        // }
     }
 }
